6c7b1586ee7036f9effbab9965a0802750258e42
daipresents <daipresents@gmail.com>
2016-08-12 00:17:12 +0900

    ViewPager 課題１． (課題) 実習1のプロジェクトを引き続き使用してください。ActionBarのTab Navigationを追加し、Tabを選択したら該当のページが表示されるようにしてください。（3番目のTabを選択したら3ページ目が表示される）また、ページをスクロールさせたら該当のTabが選択されるようにしてください。 また、Tabとページの数は3つとしてください。 => DONE.

--- AndroidStudio/projects/fundamentals/6th/ViewPagerSample/app/src/main/java/jp/mixi/sample/viewpager/MainActivity.java
+++ AndroidStudio/projects/fundamentals/6th/ViewPagerSample/app/src/main/java/jp/mixi/sample/viewpager/MainActivity.java
@@ -1,7 +1,9 @@
 
 package jp.mixi.sample.viewpager;
 
+import android.app.ActionBar;
 import android.app.Activity;
+import android.app.FragmentTransaction;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
@@ -13,7 +15,7 @@ import android.view.Menu;
  * (実習) FragmentPagerAdapter の使い方で説明したプロジェクトを写経して実行してください。
  * 説明したプロジェクトはAndroidTraining/projects/fundamentals/6th/FragmentViewPagerSampleにあります。
  */
-public class MainActivity extends FragmentActivity {
+public class MainActivity extends FragmentActivity implements ActionBar.TabListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +29,28 @@ public class MainActivity extends FragmentActivity {
 
         pager.setAdapter(new SamplePagerAdapter(this));
 
+        /**
+         * TODO
+         * (課題) 実習1のプロジェクトを引き続き使用してください。
+         * ActionBarのTab Navigationを追加し、Tabを選択したら該当のページが表示されるようにしてください。
+         * （3番目のTabを選択したら3ページ目が表示される）
+         * また、ページをスクロールさせたら該当のTabが選択されるようにしてください。
+         * また、Tabとページの数は3つとしてください。
+         */
+        // タブナビゲーションモードに設定
+        getActionBar().setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
+        // タブを作成して追加。タブの選択・解除・再選択をハンドリングするコールバックの TabListener をセットしないと実行時例外でクラッシュする
+        getActionBar().addTab(getActionBar().newTab().setText("Tab1").setTabListener(this));
+        getActionBar().addTab(getActionBar().newTab().setText("Tab2").setTabListener(this));
+        getActionBar().addTab(getActionBar().newTab().setText("Tab3").setTabListener(this));
+
+        //スワイプしたときにもActionbarのタブ（NavigationItem）を常に表示させる処理
+        pager.setOnPageChangeListener(new ViewPager.SimpleOnPageChangeListener() {
+            @Override
+            public void onPageSelected(int position) {
+                getActionBar().setSelectedNavigationItem(position);
+            }
+        });
     }
 
     @Override
@@ -36,4 +60,19 @@ public class MainActivity extends FragmentActivity {
         return true;
     }
 
+    // タブナビゲーションの Tab が選択された時のコールバック
+    @Override
+    public void onTabSelected(ActionBar.Tab tab, FragmentTransaction ft) {
+
+    }
+
+    // タブナビゲーションの Tab が選択解除された時のコールバック
+    @Override
+    public void onTabUnselected(ActionBar.Tab tab, FragmentTransaction ft) {
+    }
+
+    // タブナビゲーションの Tab が再度選択された時のコールバック
+    @Override
+    public void onTabReselected(ActionBar.Tab tab, FragmentTransaction ft) {
+    }
 }
