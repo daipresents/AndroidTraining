2721747d4ebd6e05421ca03d5fedd8503b1b8a2d
daipresents <daipresents@gmail.com>
2016-08-19 02:09:28 +0900

    (課題) youtubeで日本で前日の評価が最も高い動画のタイトルをAPIで取得し表示してください。 => DONE. YoutubeのAPIが「No longer available」返すけど動いてる？

--- AndroidStudio/assignments/fundamentals/9th/NetworkAssignment/app/src/main/java/jp/mixi/assignment/network/networkAssignment/MainActivity.java
+++ AndroidStudio/assignments/fundamentals/9th/NetworkAssignment/app/src/main/java/jp/mixi/assignment/network/networkAssignment/MainActivity.java
@@ -4,9 +4,23 @@ import android.app.Activity;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.StrictMode;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.Loader;
+import android.util.Log;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class MainActivity extends Activity {
+/**
+ * TODO
+ * (課題) youtubeで日本で前日の評価が最も高い動画のタイトルをAPIで取得し表示してください。
+ */
+public class MainActivity extends FragmentActivity implements LoaderManager.LoaderCallbacks<String> {
+
+    Activity mainActivity;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,7 +34,35 @@ public class MainActivity extends Activity {
         }
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mainActivity = this;
+
+        LoaderManager manager = getSupportLoaderManager();
+        Bundle argsForLoader = new Bundle();
+        manager.initLoader(0, argsForLoader, MainActivity.this);
+
+
     }
+
+    @Override
+    public Loader<String> onCreateLoader(int id, Bundle args) {
+        switch (id) {
+            case 0:
+                return new GetAsyncTaskLoader(this);
+            default:
+                return null;
+        }
+    }
+
+    @Override
+    public void onLoadFinished(Loader<String> loader, String result) {
+        Log.v(MainActivity.class.getSimpleName(), result);
+        TextView response = (TextView) mainActivity.findViewById(R.id.title);
+        response.setText(result);
+    }
+
+    @Override
+    public void onLoaderReset(Loader<String> loader) {}
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
