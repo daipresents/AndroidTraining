80140a88d7f9d4ccab66757b526c9a2fd27440b8
daipresents <daipresents@gmail.com>
2016-08-20 04:20:30 +0900

    Dialog (実習) DialogPractice2 のプロジェクトに、Activity と Fragment が配置されています。TODO コメントの指示通りのプログラムを記述してください。 => DONE.

--- AndroidStudio/practice/advanced/3rd/DialogPractice2/app/src/main/java/jp/mixi/practice/dialog/med/ListItemSelectionDialogFragment.java
+++ AndroidStudio/practice/advanced/3rd/DialogPractice2/app/src/main/java/jp/mixi/practice/dialog/med/ListItemSelectionDialogFragment.java
@@ -1,6 +1,18 @@
 package jp.mixi.practice.dialog.med;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.DialogFragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
 /**
  * TODO: ダイアログで、はい・いいえ の選択肢を表示する
@@ -9,5 +21,41 @@ import android.support.v4.app.DialogFragment;
  *
  */
 public class ListItemSelectionDialogFragment extends DialogFragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return null;
+    }
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+
+        // AlertDialogはBuilderパターンで生成
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+
+        // TODO: ダイアログで、はい・いいえ の選択肢を表示する
+        builder.setMessage("Delete?")
+                // OKボタン
+                .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        // TODO: はい、を選択したら、リストの項目を削除する
+                        Log.v(ListItemSelectionDialogFragment.class.getSimpleName(), "OK.");
+
+                        ListView listView = (ListView) getActivity().findViewById(R.id.ListView);
+                        String target = (String) listView.getItemAtPosition(getArguments().getInt("position"));
+                        ArrayAdapter<String> adapter = (ArrayAdapter<String>) listView.getAdapter();
+                        adapter.remove(target);
+                    }
+                })
+                // Cancelボタン
+                .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        Log.v(ListItemSelectionDialogFragment.class.getSimpleName(), "Canceled.");
+                    }
+                });
+        // Dialogを作成して返却
+        return builder.create();
+    }
 
 }
--- AndroidStudio/practice/advanced/3rd/DialogPractice2/app/src/main/java/jp/mixi/practice/dialog/med/MainActivity.java
+++ AndroidStudio/practice/advanced/3rd/DialogPractice2/app/src/main/java/jp/mixi/practice/dialog/med/MainActivity.java
@@ -3,14 +3,24 @@ package jp.mixi.practice.dialog.med;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
+import android.support.v4.app.FragmentActivity;
+import android.util.Log;
 import android.view.ContextMenu;
 import android.view.ContextMenu.ContextMenuInfo;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Adapter;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
-public class MainActivity extends Activity {
+/**
+ * TODO
+ * (実習) DialogPractice2 のプロジェクトに、Activity と Fragment が配置されています。
+ * TODO コメントの指示通りのプログラムを記述してください。
+ */
+public class MainActivity extends FragmentActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -45,7 +55,7 @@ public class MainActivity extends Activity {
     public void onCreateContextMenu(ContextMenu menu, View v, ContextMenuInfo menuInfo) {
         super.onCreateContextMenu(menu, v, menuInfo);
 
-        // TODO: 長押しメニューに、削除・キャンセル、の 2 つの項目を表示する
+        // TODO: 長押しメニューに、削除・キャンセル、の 2 つの項目を表示する => DONE.
         getMenuInflater().inflate(R.menu.main, menu);
     }
 
@@ -53,6 +63,21 @@ public class MainActivity extends Activity {
     public boolean onContextItemSelected(MenuItem item) {
         // TODO: 長押しメニューの、削除、の項目の選択をハンドリングして、確認のためのダイアログを
         // ListItemSelectionDialogFragment を使用して表示する
+
+        // MenuItemからContextMenuInfoを取得し、AdapterContextMenuInfoにキャストします
+        ContextMenuInfo menuInfo = item.getMenuInfo();
+        AdapterView.AdapterContextMenuInfo adapterInfo = (AdapterView.AdapterContextMenuInfo) menuInfo;
+
+        // AdapterContextMenuInfoから長押ししたリストアイテムのpositionを取得します
+        int position = adapterInfo.position;
+
+        DialogFragment fragment = new ListItemSelectionDialogFragment();
+        Bundle bundle = new Bundle();
+        bundle.putInt("position", position);
+        fragment.setArguments(bundle);
+        // 引数にFramentManagerとtagを設定します
+        fragment.show(getSupportFragmentManager(), "list_item_selection_dialog_fragment");
+
         return super.onContextItemSelected(item);
     }
 }
